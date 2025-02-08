7544b287ae59db4d2cb9a806d3c01203874b2f13
Cassiano-lamarc96 <cacassiano2011@hotmail.com>
2025-02-08 12:46:55 -0300

    Adicionar arquivos de projeto.

---
+++ Capitals/Program.cs
@@ -0,0 +1,8 @@
+﻿while (true)
+{
+    Console.WriteLine("Type a word: ");
+    var word = Console.ReadLine();
+
+    Console.WriteLine(word == null || word.Length == 0 ? false : (word.All(char.IsUpper)) || (word.All(char.IsLower)) ||
+        (word[0].ToString().All(char.IsUpper) && (word.Length == 1 || word.Substring(1, word.Length - 1).All(char.IsLower))) ? true : false);
+}
